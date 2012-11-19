9dadd81febf68bcad3c9b369a2c72a8c4ce694ea
Dmitry Vedenko <vedenko@gmail.com>
2012-11-20 01:26:50 +0400

    Basic changes to the UI

--- TheGame/TheGame/MainWindow.xaml.cs
+++ TheGame/TheGame/MainWindow.xaml.cs
@@ -14,25 +14,7 @@ namespace TheGame
         }
 
 
-        private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            ColoredButton.Background = new SolidColorBrush(Color.FromRgb((byte)RedSlider.Value, (byte)GreedSlider.Value, (byte)BlueSlider.Value));
-            ColoredButton.Content = "Red Value = " +   (byte) RedSlider.Value +
-                "Green Value = " + (byte) GreedSlider.Value +
-                                    "Blue Value = " + (byte)BlueSlider.Value;
-        }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
         {
 
         }
